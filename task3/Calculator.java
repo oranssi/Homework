38321623ad44b80bcd1f1f015b22f6afe0932147
Viktor <Viktor@MacBook-Pro-Viktor.local>
2018-05-06 12:11:54 +0200

    Task3 commit

---
+++ task3/Calculator.java
@@ -0,0 +1,25 @@
+package task3;
+
+/**
+ * Created by Viktor on 15.04.2018.
+ */
+public class Calculator {
+
+    public float adding(int a, int b) {
+        return a + b;
+    }
+
+    public float subtract(int a, int b) {
+        return a - b;
+    }
+
+    public float multiply(int a, int b) {
+        return a * b;
+    }
+
+    public float divide(int a, int b) {
+        return a / b;
+    }
+
+
+}
